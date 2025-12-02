dcdf2d59bd831ef3465d77803bf8f1433b6eff72
Jael Contreras <contrerasJael061@gmail.com>
2025-12-02 15:47:41 -0600

    Ajustando rutas a backend

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
         submitBtn.disabled = true;
 
         try {
-            const response = await fetch('http://localhost:3000/tech-up/users/login', {
+            const response = await fetch('http://tech-up.westus2.cloudapp.azure.com:3000/tech-up/users/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
